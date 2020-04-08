19a6d8a8985a30e309387545077cf178db5db2f3
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 21:03:57 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,9 +1,12 @@
 def count_elements(array)
   # code goes here
   my_hash = {}
-  uniq_array = array.uniq
-  uniq_array.each do |item|
-    my_hash[item] = 0
+  array.each do |item|
+    if my_hash[item] == nil
+      my_hash[item] = 1 
+    else
+      my_hash[item] += 1
+    end
   end
   my_hash
 end
