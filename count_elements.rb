9e97bd3217e4413d351d7a14933d114957aee42a
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 21:04:00 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,7 +3,7 @@ def count_elements(array)
   my_hash = {}
   array.each do |item|
     if my_hash[item] == nil
-      my_hash[item] = 1 
+      my_hash[item] = 1
     else
       my_hash[item] += 1
     end
